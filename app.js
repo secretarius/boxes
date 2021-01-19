de6571f43eb04d0031585d91e2453bcd368c0c34
Oleksandr Maysak <olexandrmai@gmail.com>
2021-01-19 11:31:42 +0200

    heroku init

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
+//Install express server
 const express = require('express');
-const http = require('http');
 const path = require('path');
-const compression = require('compression'); 
 
 const app = express();
 
-app.use(compression());
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/box'));
 
-app.use(express.static(path.join(__dirname, 'dist/box')));
+app.get('/*', function(req,res) {
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/box/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/box/index.html'));
 });
 
-const port = process.env.PORT || 3000;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log("running"));
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
